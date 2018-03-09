7cc4ddc4c4fb93f73601bd4efedd48d3a0057cc7
HiPPO <humanitystruth>
2018-03-08 18:17:41 -0800

    adding rip blade

---
+++ resources/views/rip.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+@section('content')
+<div class="w3-container w3-padding w3-card-4 w3-round-large w3-margin w3-white">
+        <h2>R.I.P. List</h2>
+        <p>Here's a list of true patriots who lost their life fighting battles of truth versus suppression. 
+           They all deserve our respect and attention.</p>
+        <ul><!-- link these tags for files -->
+            <li>Nikola Tesla</li>
+            <li>President J.F.K.</li>
+            <li>Stan Meyer</li>
+            <li>Philip Schneider</li>
+            <li>...</li>
+            
+        </ul>
+        
+        <p>Here's a list of orchestrated world events, where we lost a battle of truth versus suppression. 
+           These situations all deserve an extended open-source investigation, so we may not be influenced by misinformation.
+        </p>
+        <ul><!-- link these tags for investigations -->
+            <li>9/11</li>
+            <li>...</li>
+            
+        </ul>
+</div>
+@stop
